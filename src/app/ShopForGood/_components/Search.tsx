b9b8b3ea0ead3fb993b818a6023ccc25703a455e
Kehinde-Agboola <kehindeagboola52@gmail.com>
2025-01-09 00:25:11 +0100

    search-component

---
+++ src/app/ShopForGood/_components/Search.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const Search = () => {
+  const fadeIn = {
+    hidden: { opacity: 0, y: 20 },
+    visible: (delay = 0) => ({
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.6, delay },
+    }),
+  };
+
+  return (
+    <motion.section
+      variants={fadeIn}
+      custom={0.4}
+      initial="hidden"
+      animate="visible"
+    >
+      <form className="pt-4">
+        <div className="relative w-[350px] md:w-[550px]">
+          <input
+            id="email"
+            type="email"
+            placeholder="Search all Categories"
+            className="focus:outline-none text-[#777777] px-6 py-3 w-full pr-28 "
+          />
+          <button
+            type="submit"
+            aria-label="Subscribe to newsletter"
+            className="bg-[#FFB400] absolute right-2 top-1/2 transform -translate-y-1/2 px-6 text-black py-2 "
+          >
+            Search
+          </button>
+        </div>
+      </form>
+    </motion.section>
+  );
+};
+
+export default Search;
