274245f8c9d5f1cbcfb884306f37d050dbad8ec4
Kehinde-Agboola <kehindeagboola52@gmail.com>
2025-04-08 18:36:56 +0100

    head-reconfig

---
+++ src/app/(staticPages)/_home/Head.tsx
@@ -0,0 +1,21 @@
+
+import React from "react";
+import { HeroComponent } from "../../_component/atom/hero";
+// import background from "../../../public/assets/home/home-hero.png"
+const Head = () => {
+  return (
+    <main className="h-full">
+      <HeroComponent
+        heading="CARE."
+        spanText="EDUCATION."
+        heading2="EMPOWERMENT."
+        // buttonText=""
+        buttonText="Get Involved"
+        image="/assets/heos.png"
+        subtext="Empowering children to be their best, recreating our future."
+      />
+    </main>
+  );
+};
+
+export default Head;
