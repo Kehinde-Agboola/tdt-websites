a6e4ab72b5b06cd92ffb424bf06c6dc77be2bfea
Kehinde-Agboola <kehindeagboola52@gmail.com>
2025-04-08 18:31:52 +0100

    tailwindcss-file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -54,8 +54,8 @@ const config: Config = {
   		},
   		backgroundImage: {
   			hero: "url('./../../public/assets/home/home-hero.png')",
-  			whero: "linear-gradient(to left, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('./../../public/assets/who/whero.png')",
-  			payment: "url('./../../public/assets/home/impact.png')",
+  			whero: "linear-gradient(to left, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('./../../../public/assets/who/whero.png')",
+  			payment: "url('./../../../public/assets/home/impact.png')",
   			community: "url('./../../public/assets/home/community.png')",
   			navbg: 'linear-gradient(90deg, rgba(53,53,53,1) 0%, rgba(127,127,127,1) 35%, rgba(255,255,255,1) 100%);',
   			mobile: 'linear-gradient(to right, rgba(53,53,53,1) 17%, rgba(127,127,127,1) 60%, rgba(255,255,255,1) 76%);'
